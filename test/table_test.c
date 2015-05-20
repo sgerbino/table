df5cdef91d4e7449dd3f8570722ec109989c74e2
Steve Gerbino <steve@gerbino.co>
2015-05-20 01:49:22 -0400

    Restructured the table unit test framework

--- test/table_test.c
+++ test/table_test.c
@@ -1,117 +1,118 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
 #include <string.h>
 #include <table.h>
 
-typedef enum _return_code {
-   PASS,
-   FAIL
-} return_code;
+typedef struct _test {
+   bool return_code;
+   const char *name;
+   bool (*function)(void);
+} test;
 
-void result(const char *entry, return_code rc);
+bool column_test(void);
+bool row_test(void);
 
-return_code add_columns(table *t);
-return_code check_columns(table *t);
+test tests[] = {
+   { true, "column", column_test },
+   { true, "row", row_test },
+   { NULL, NULL, NULL }
+};
 
-return_code add_rows(table *t);
-return_code check_rows(table *t);
-
-return_code add_data(table *t);
-return_code check_data(table *t);
+void run_test(test *t);
+void print_header(const char *title);
 
 int main(int argc, char **argv)
 {
-   table *t = table_new();
-   int i;
+   const char *title = "Table Unit Tests";
+   test *t;
 
-   printf("table test\n");
-   for (i = 0; i < 34; i++) putchar('-');
+   print_header(title);
+   for (t = tests; t->name; ++t)
+      run_test(t);
+
+   for (t = tests; t->name; ++t)
+      if (!t->return_code)
+	 return -1;
+   
+   return 0;
+}
+
+void print_header(const char *title)
+{
+   size_t i;
+   size_t len = strlen(title);
+   printf("%s\n", title);
+   for (i = 0; i < len; ++i) putchar('-');
    putchar('\n');
-
-   result("column check", add_columns(t) | check_columns(t));
-   result("row check", add_rows(t) | check_rows(t));
-   result("data check", add_data(t) | check_data(t));
-
-   table_delete(t);
-   return 0;
 }
 
-void result(const char *entry, return_code rc)
+void run_test(test *t)
 {
-   printf("%-25s [ %4s ]\n", entry, PASS == rc ? "PASS" : "FAIL");
+   clock_t c;
+   double elapsed;
+   char buf[64];
+   c = clock();
+   snprintf(buf, sizeof buf, "Running %s test...", t->name);
+   printf("%-40s", buf);
+   t->return_code = t->function();
+   c = clock() - c;
+   elapsed = (double)c/CLOCKS_PER_SEC*1000;
+   printf(" %.3fms [ %4s ]\n", elapsed, t->return_code ? "PASS" : "FAIL");
 }
 
-return_code add_columns(table *t)
+bool column_test(void)
 {
-   table_add_column(t, "id", TABLE_INT);
-   table_add_column(t, "name", TABLE_STRING);
-   return 0;
-}
+   table t;
+   int num_cols, id_col, name_col;
+   bool rc = true;
 
-return_code check_columns(table *t)
-{
-   return_code rc = PASS;
-   int num_cols = table_get_column_length(t);
+   table_init(&t);
+   
+   id_col = table_add_column(&t, "id", TABLE_INT);
+   name_col = table_add_column(&t, "name", TABLE_STRING);
+
+   num_cols = table_get_column_length(&t);
 
    if (num_cols != 2)
-      rc = FAIL;
+      rc = false;
 
-   if (strcmp(table_get_column_name(t, 0), "id"))
-      rc = FAIL;
+   if (strcmp(table_get_column_name(&t, id_col), "id"))
+      rc = false;
 
-   if (strcmp(table_get_column_name(t, 1), "name"))
-      rc = FAIL;
+   if (strcmp(table_get_column_name(&t, name_col), "name"))
+      rc = false;
+
+   table_destroy(&t);
 
    return rc;
 }
 
-return_code add_rows(table *t)
+bool row_test(void)
 {
-   table_add_row(t);
-   table_add_row(t);
-   table_add_row(t);
-   return PASS;
-}
+   table t;
+   time_t now;
+   int i, num_rows, random_number;
+   bool rc = true;
+   const size_t range = 100;
+   const size_t minimum = 100;
 
-return_code check_rows(table *t)
-{
-   return_code rc = PASS;
-   if (table_get_row_length(t) != 3)
-      rc = FAIL;
-   return rc;
-}
+   srand(time(&now));
+   
+   random_number = rand() % range + minimum;
 
-return_code add_data(table *t)
-{
-   int num_rows = table_get_row_length(t);
-   int row;
+   table_init(&t);
 
-   for (row = 0; row < num_rows; row++)
-   {
-      char buf[255];
-      snprintf(buf, 255, "name %d", row);
-      table_set_int(t, row, 0, row);
-      table_set_string(t, row, 1, buf);
-   }
+   for (i = 0; i < random_number; ++i)
+      table_add_row(&t);
 
-   return PASS;
-}
+   num_rows = table_get_row_length(&t);
 
-return_code check_data(table *t)
-{
-   return_code rc = PASS;
-   int num_rows = table_get_row_length(t);
-   int row;
+   if (num_rows != random_number)
+      rc = false;
 
-   for (row = 0; row < num_rows; row++)
-   {
-      char buf[255];
-      snprintf(buf, 255, "name %d", row);
+   table_destroy(&t);
 
-      if (table_get_int(t, row, 0) != row)
-         rc = FAIL;
-
-      if (strcmp(table_get_string(t, row, 1), buf))
-         rc = FAIL;
-   }
    return rc;
 }
