9142f3e784f356aa08e27b239cb1d78b7d32933b
Steve Gerbino <steve@gerbino.co>
2015-05-21 22:48:58 -0400

    Added a table validator test

--- test/table_test.c
+++ test/table_test.c
@@ -15,11 +15,13 @@ typedef struct _test {
 bool column_test(char *buf, size_t len);
 bool row_test(char *buf, size_t len);
 bool callback_test(char *buf, size_t len);
+bool validator_test(char *buf, size_t len);
 
 static test tests[] = {
    { true, "column", column_test, {0} },
    { true, "row", row_test, {0} },
    { true, "callback", callback_test, {0} },
+   { true, "validator", validator_test, {0} },
    { false, NULL, NULL, {0} }
 };
 
@@ -187,5 +189,61 @@ bool callback_test(char *buf, size_t len)
 
    table_unregister_callback(&t, table_callback, &result);
    table_destroy(&t);
+   return rc;
+}
+
+bool validator_test(char *buf, size_t len)
+{
+   bool rc = true;
+   int row;
+   int col;
+   table t;
+   time_t now;
+
+   srand(time(&now));
+   row = rand();
+   srand(time(&now));
+   col = rand();
+
+   table_init(&t);
+   if (table_row_is_valid(&t, row))
+   {
+      snprintf(buf, len, "Row %d should not be valid", row);
+      rc = false;
+   }
+
+   if (table_column_is_valid(&t, col))
+   {
+      snprintf(buf, len, "Column %d should not be valid", col);
+      rc = false;
+   }
+
+   if (table_cell_is_valid(&t, row, col))
+   {
+      snprintf(buf, len, "Cell %d, %d should not be valid", row, col);
+      rc = false;
+   }
+
+   row = table_add_row(&t);
+   if (!table_row_is_valid(&t, row))
+   {
+      snprintf(buf, len, "Row %d should be valid", row);
+      rc = false;
+   }
+
+   col = table_add_column(&t, "id", TABLE_INT);
+   if (!table_column_is_valid(&t, col))
+   {
+      snprintf(buf, len, "Column %d should be valid", col);
+      rc = false;
+   }
+
+   if (!table_cell_is_valid(&t, row, col))
+   {
+      snprintf(buf, len, "Cell %d, %d should be valid", row, col);
+      rc = false;
+   }
+   table_destroy(&t);
+
    return rc;
 }
