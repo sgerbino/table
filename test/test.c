c5aeba5cd981f8f6a080a2b34f5f28582ba588c3
Steve Gerbino <steve@gerbino.co>
2015-12-15 21:55:58 -0500

    Added ANSI color escape sequences to unit test output, moved CMake install calls to their respective modules

--- test/test.c
+++ test/test.c
@@ -1,4 +1,29 @@
+#include <unistd.h>
+#include <stdio.h>
 #include "test.h"
+
+#define NRM  "\x1B[0m"
+#define RED  "\x1B[31m"
+#define GRN  "\x1B[32m"
+#define YEL  "\x1B[33m"
+#define BLU  "\x1B[34m"
+#define MAG  "\x1B[35m"
+#define CYN  "\x1B[36m"
+#define WHT  "\x1B[37m"
+
+static const char *test_pass_string()
+{
+	if (isatty(STDOUT_FILENO))
+		return GRN"PASS"NRM;
+	return "PASS";
+}
+
+static const char *test_fail_string()
+{
+	if (isatty(STDOUT_FILENO))
+		return RED"FAIL"NRM;
+	return "FAIL";
+}
 
 /**
  * \brief Prints the test title to standard out
@@ -31,7 +56,7 @@ bool test_run(test *t)
    return_code = t->function(t->buf, sizeof t->buf);
    c = clock() - c;
    elapsed = (double)c/CLOCKS_PER_SEC*1000;
-   printf(" %.3fms [ %4s ]\n", elapsed, return_code ? "PASS" : "FAIL");
+   printf(" %.3fms [ %4s ]\n", elapsed, return_code ? test_pass_string() : test_fail_string());
    
    if (strlen(t->buf) > 0)
       printf(" `-> %s\n", t->buf);
