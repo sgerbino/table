1fe92a7382ccb84a7124a0586c6de25ac1fc2eba
Steve Gerbino <steve@gerbino.co>
2017-01-03 00:39:50 -0500

    Fix for loop initial declaration for compilation in gcc

--- test/table_memtest.c
+++ test/table_memtest.c
@@ -14,20 +14,21 @@ int main(int argc, char **argv)
 	const int NUM_ROWS = 500;
 	table_bitfield result = 0;
 	time_t now;
+	int column_index, row, num_cols;
 	table *t = table_new();
 
 	srand((unsigned int)time(&now));
 
 	table_register_callback(t, table_callback, &result, ~0);
 	
-	for (int column_index = 0; column_index < NUM_DATA_TYPES; column_index++) {
+	for (column_index = 0; column_index < NUM_DATA_TYPES; column_index++) {
 		char name[2] = {0};
 		name[0] = (char)column_index;
 		table_add_column(t, name, column_index);
 	}
 
-	int num_cols = table_get_column_length(t);
-	for (int row = 0; row < NUM_ROWS; row++) {
+	num_cols = table_get_column_length(t);
+	for (row = 0; row < NUM_ROWS; row++) {
 		size_t data_size = 64;
 		char *data = malloc(data_size);
 
